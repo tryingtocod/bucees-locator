65b713b41a828e97daf9c898c5596e983b6e5422
tryingtocod <sara.m.inacio.tome@gmail.com>
2025-08-26 15:56:28 -0300

    Add img zzz

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-// Centralizar nos EUA (Texas mais ou menos no centro)
-var map = L.map('map').setView([31.9686, -99.9018], 5);
+// Criar o mapa centralizado nos EUA
+var map = L.map('map').setView([31.0, -97.0], 5);
 
-// Camada de mapa (OpenStreetMap gratuito)
+// Adicionar camada base (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Carregar JSON com as lojas Buc-ee's
+// Criar o ícone personalizado do Buc-ee’s
+var buceesIcon = L.icon({
+  iconUrl: 'img/bucees-logo.png',  // Caminho do logo
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -40]
+});
+
+// Carregar os dados do JSON (usando bucees.json)
 fetch('bucees.json')
   .then(response => response.json())
-  .then(stores => {
-    stores.forEach(store => {
-      L.marker([store.latitude, store.longitude])
+  .then(data => {
+    data.forEach(store => {
+      // Criar marcador com logo do Buc-ee’s
+      L.marker([store.latitude, store.longitude], { icon: buceesIcon })
         .addTo(map)
-        .bindPopup(`<b>${store.name}</b>`);
+        .bindPopup(`<b>${store.name}</b><br>${store.address}`);
     });
   })
   .catch(error => console.error('Erro ao carregar JSON:', error));
